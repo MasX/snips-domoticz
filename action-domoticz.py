cb333e91f74a540057bd2156a93175bcc0214e1b
Erik <github@masx.nl>
2019-04-05 13:24:33 +0200

    call api, include config

--- action-domoticz.py
+++ action-domoticz.py
@@ -7,19 +7,41 @@ https://snipskit.readthedocs.io/
 """
 from snipskit.hermes.apps import HermesSnipsApp
 from snipskit.hermes.decorators import intent
+import requests
 
 
 class Domoticz(HermesSnipsApp):
 
     @intent('MasX:OpenShutters')
     def open_shutters(self, hermes, intent_message):
-        hermes.publish_end_session(intent_message.session_id,
-                                   'I received OpenShutters')
+        app_config = AppConfig()
+        hostname = app_config['secret']['hostname']
+        switch_no = app_config['secret']['switch_no']
+        success = self.set_shutter_level(hostname, switch_no, 1)
+        if success:
+            hermes.publish_end_session(intent_message.session_id,
+                                       'Shutters opened.')
+        else:
+            hermes.publish_end_session(intent_message.session_id,
+                                       'Had an issue opening the shutters.')
 
     @intent('MasX:CloseShutters')
     def close_shutters(self, hermes, intent_message):
-        hermes.publish_end_session(intent_message.session_id,
-                                   'I received CloseShutters')
+        app_config = AppConfig()
+        hostname = app_config['secret']['hostname']
+        switch_no = app_config['secret']['switch_no']
+        success = self.set_shutter_level(hostname, switch_no, 99)
+        if success:
+            hermes.publish_end_session(intent_message.session_id,
+                                       'Shutters closed.')
+        else:
+            hermes.publish_end_session(intent_message.session_id,
+                                       'Had an issue closing the shutters.')
+
+    def set_shutter_level(self, hostname, switch_no, level):
+        r = requests.get("%s/json.htm?type=command&param=switchlight&idx=%s&switchcmd=Set%20Level&level=%s") \
+            % (hostname, switch_no, level)
+        return r.status_code == 200
 
 
 if __name__ == "__main__":
